695225b81549d059f712a20e1e9086cf65f29a6f
Maximiliano <maximiliano.condurzo@gmail.com>
2016-04-12 18:13:06 -0300

    Comit

--- Assets/Easy Code Scanner v2.0/EasyCodeScannerExample.cs
+++ Assets/Easy Code Scanner v2.0/EasyCodeScannerExample.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 using System.Collections;
 using System.Runtime.InteropServices;
 using System;
-
+using UnityEngine.UI;
 public class EasyCodeScannerExample : MonoBehaviour {
 	
 	static string dataStr;
-	public Renderer PlaneRender;
+	//public Renderer PlaneRender;
+	public Text nombreProducto;
 	
 	void Start () {
 		dataStr = "";
@@ -32,50 +33,56 @@ public class EasyCodeScannerExample : MonoBehaviour {
 	
 	public void Update() {
 			
-		if (Input.GetKeyDown(KeyCode.Escape)) { 
-			Application.Quit();
-		}
-		
-	}
+//		if (Input.GetKeyDown(KeyCode.Escape)) { 
+//			Application.Quit();
+//		}
 	
-	void OnGUI ()
-	{
-		//--- To be replaced by your controls, just to showcase ---
-		if (GUI.Button(new Rect (10, 125, 300, 100), "Launch scanner"))
-		{
-			//for code list, see : http://zbar.sourceforge.net/api/zbar_8h.html#f7818ad6458f9f40362eecda97acdcb0
-			// and zbar.h 
-			/*
-			typedef enum zbar_symbol_type_e {
-		    ZBAR_NONE        =      0,  < no symbol decoded 
-		    ZBAR_PARTIAL     =      1,  < intermediate status 
-		    ZBAR_EAN8        =      8,  < EAN-8 /
-		    ZBAR_UPCE        =      9,  < UPC-E 
-		    ZBAR_ISBN10      =     10,  < ISBN-10 (from EAN-13). @since 0.4 
-		    ZBAR_UPCA        =     12,  < UPC-A 
-		    ZBAR_EAN13       =     13,  < EAN-13 
-		    ZBAR_ISBN13      =     14,  < ISBN-13 (from EAN-13). @since 0.4 
-		    ZBAR_I25         =     25,  < Interleaved 2 of 5. @since 0.4 
-		    ZBAR_CODE39      =     39,  < Code 39. @since 0.4 
-		    ZBAR_PDF417      =     57,  < PDF417. @since 0.6 
-		    ZBAR_QRCODE      =     64,  < QR Code. @since 0.10 
-		    ZBAR_CODE128     =    128,  < Code 128 
-		    ZBAR_SYMBOL      = 0x00ff,  < mask for base symbol type 
-		    ZBAR_ADDON2      = 0x0200,  < 2-digit add-on flag 
-		    ZBAR_ADDON5      = 0x0500,  < 5-digit add-on flag 
-		    ZBAR_ADDON       = 0x0700,  < add-on flag mask 
-			} zbar_symbol_type_t;
-			*/
-			//for instance, only QRCodes
-			//launchScanner( true, "Scanning...", 64, true);
-			
-			//all codes at the same time : value = -1
-			EasyCodeScanner.launchScanner( true, "Scanning...", -1, true);
-		}
-	
-		dataStr = GUI.TextArea(new Rect (10, 275, 300, 100), dataStr);
-		//---
+		nombreProducto.text = dataStr;
 	}
+
+	public void ShowCam(){
+		EasyCodeScanner.launchScanner( true, "Scanee un codigo de Barras", -1, true);
+	}
+
+
+//	void OnGUI ()
+//	{
+//		//--- To be replaced by your controls, just to showcase ---
+//		if (GUI.Button(new Rect (10, 125, 300, 100), "Launch scanner"))
+//		{
+//			//for code list, see : http://zbar.sourceforge.net/api/zbar_8h.html#f7818ad6458f9f40362eecda97acdcb0
+//			// and zbar.h 
+//			/*
+//			typedef enum zbar_symbol_type_e {
+//		    ZBAR_NONE        =      0,  < no symbol decoded 
+//		    ZBAR_PARTIAL     =      1,  < intermediate status 
+//		    ZBAR_EAN8        =      8,  < EAN-8 /
+//		    ZBAR_UPCE        =      9,  < UPC-E 
+//		    ZBAR_ISBN10      =     10,  < ISBN-10 (from EAN-13). @since 0.4 
+//		    ZBAR_UPCA        =     12,  < UPC-A 
+//		    ZBAR_EAN13       =     13,  < EAN-13 
+//		    ZBAR_ISBN13      =     14,  < ISBN-13 (from EAN-13). @since 0.4 
+//		    ZBAR_I25         =     25,  < Interleaved 2 of 5. @since 0.4 
+//		    ZBAR_CODE39      =     39,  < Code 39. @since 0.4 
+//		    ZBAR_PDF417      =     57,  < PDF417. @since 0.6 
+//		    ZBAR_QRCODE      =     64,  < QR Code. @since 0.10 
+//		    ZBAR_CODE128     =    128,  < Code 128 
+//		    ZBAR_SYMBOL      = 0x00ff,  < mask for base symbol type 
+//		    ZBAR_ADDON2      = 0x0200,  < 2-digit add-on flag 
+//		    ZBAR_ADDON5      = 0x0500,  < 5-digit add-on flag 
+//		    ZBAR_ADDON       = 0x0700,  < add-on flag mask 
+//			} zbar_symbol_type_t;
+//			*/
+//			//for instance, only QRCodes
+//			//launchScanner( true, "Scanning...", 64, true);
+//			
+//			//all codes at the same time : value = -1
+//			EasyCodeScanner.launchScanner( true, "Scanning...", -1, true);
+//		}
+//	
+//		dataStr = GUI.TextArea(new Rect (10, 275, 300, 100), dataStr);
+//		//---
+//	}
 	
 	//Callback when returns from the scanner
 	void onScannerMessage(string data){
